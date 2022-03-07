7d3431dff473ff2b5ec0430c159068112699c3b2
Ryan Wenger <ryanwenger@umail.ucsb.edu>
2022-03-07 13:31:48 -0800

    rw - integrate CommonsController class & tests with updated entity fields

--- src/main/java/edu/ucsb/cs156/happiercows/controllers/CommonsController.java
+++ src/main/java/edu/ucsb/cs156/happiercows/controllers/CommonsController.java
@@ -73,7 +73,13 @@ public class CommonsController extends ApiController {
   public ResponseEntity<String> createCommons(@ApiParam("name of commons") @RequestBody CreateCommonsParams params)
       throws JsonProcessingException {
     log.info("name={}", params.getName());
-    Commons c = Commons.builder().name(params.getName()).build();
+    Commons c = Commons.builder()
+            .name(params.getName())
+            .cowPrice(params.getCowPrice())
+            .milkPrice(params.getMilkPrice())
+            .startingBalance(params.getStartingBalance())
+            .startDate(params.getStartDate())
+            .build();
     Commons savedCommons = commonsRepository.save(c);
     String body = mapper.writeValueAsString(savedCommons);
     log.info("body={}", body);
